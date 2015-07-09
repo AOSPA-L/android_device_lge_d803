bda8ad121404d4c693abca4fd8e0b8674a6b7d5b
Michael S Corigliano <michael.s.corigliano@gmail.com>
2015-07-09 16:39:56 +0000

    Adapt for AOSPA
    Conflicts:
    	pa.mk

--- pa.mk
+++ pa.mk
@@ -2,18 +2,18 @@
 TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common PA stuff.
+$(call inherit-product, vendor/pa/config/common_full_phone.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/pa/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/d803/d803.mk)
 
 ## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := d803
-PRODUCT_NAME := cm_d803
+PRODUCT_DEVICE := d801
+PRODUCT_NAME := pa_d801
 PRODUCT_BRAND := LGE
 PRODUCT_MODEL := LG-D803
 PRODUCT_MANUFACTURER := lge
@@ -23,3 +23,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 	PRIVATE_BUILD_DESC="g2_ca-user 4.4.2 KOT49I D803B20e.1394602420 release-keys"
 
 PRODUCT_PACKAGES += Torch
+    BUILD_FINGERPRINT=lge/g2_tmo_us/g2:5.0.2/LRX22G/1507914346dda:user/release-keys \
+    PRIVATE_BUILD_DESC="g2_tmo_us-user 5.0.2 LRX22G 1507914346dda release-keys"
